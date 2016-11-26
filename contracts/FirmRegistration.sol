4e19c9d4e8cbd29e9d484d0c05b496f261acddbc
Leonard Fiadzinu <leo@gmgltd.com>
2016-11-26 12:21:17 -0500

    cleaned up so stupid mistakes

--- contracts/FirmRegistration.sol
+++ contracts/FirmRegistration.sol
@@ -23,52 +23,52 @@ contract owned{
 // contract for broker registeration
 contract FirmRegistration is owned {
 
-mapping (address => FirmRegister) names;
-mapping (address => FirmStatus) status;
-//mapping ()
+  mapping (address => FirmRegister) names;
+  mapping (address => FirmStatus) status;
+  //mapping ()
 
-struct FirmRegister {
-  string name;
-  uint telephone;
-  address pubAddress;
-  bytes32 physicalAddress
-  string category;
-  string category2;
-  
-}
+  struct FirmRegister {
+    string name;
+    uint telephone;
+    address pubAddress;
+    bytes32 physicalAddress;
+    string category;
+    string category2;
+    
+  }
 
-struct FirmStatus {
-  bool suppended;
-  bool approved;
-}
+  struct FirmStatus {
+    bool suppended;
+    bool approved;
+  }
 
-struct  DealAddress{
-  mapping (address => uint) contractIDs;
-  string dealtype;
-}
+  struct  DealAddress{
+    mapping (address => uint) contractIDs;
+    string dealtype;
+  }
 
-// event logs
-event logRegisterFirm(address indexed name, string status );
+  // event logs
+  event logRegisterFirm(address indexed name, bool status );
 
-function registerFirm(address addr, string _name, uint _telephone, bytes32 _physicaladdress, string _reg_category, string _reg_category2, bool _approved, bool _stat ) onlyowner returns (bool) {
-  names[addr] = FirmRegister({
-    name: _name,
-    telephone: _telephone,
-    pubAddress: addr,
-    physicalAddress: _physicaladdress,
-    category: _reg_category,
-    category2: _reg_category2
+  function registerFirm(address addr, string _name, uint _telephone, bytes32 _physicaladdress, string _reg_category, string _reg_category2, bool _approved, bool _stat ) onlyowner returns (bool) {
+    names[addr] = FirmRegister({
+      name: _name,
+      telephone: _telephone,
+      pubAddress: addr,
+      physicalAddress: _physicaladdress,
+      category: _reg_category,
+      category2: _reg_category2
 
-  });
+    });
 
-  status[addr] = FirmStatus ({
-    suppended: _suspended,
-    approved: _approved
-  });
+    status[addr] = FirmStatus ({
+      suppended: _stat,
+      approved: _approved
+    });
 
 
-  logRegisterFirm(address addr, string status )
-  
-}
+    logRegisterFirm(addr, status[addr].approved );
+    
+  }
 
 }
